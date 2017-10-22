4827d973b7ffb825e63c3966585c3e101deca370
Kamikid <kidfu@live.com>
2017-10-22 18:13:22 +0800

    Add one self-designed exception class

--- src/exception/IllegalDataException.java
+++ src/exception/IllegalDataException.java
@@ -1,4 +1,29 @@
 package exception;
 
-public class IllegalDataException {
+public class IllegalDataException extends Exception{
+    private String retCd ;
+    private String msgDes;
+
+    public IllegalDataException() {
+        super();
+    }
+
+    public IllegalDataException(String message) {
+        super(message);
+        msgDes = message;
+    }
+
+    public IllegalDataException(String retCd, String msgDes) {
+        super();
+        this.retCd = retCd;
+        this.msgDes = msgDes;
+    }
+
+    public String getRetCd() {
+        return retCd;
+    }
+
+    public String getMsgDes() {
+        return msgDes;
+    }
 }
